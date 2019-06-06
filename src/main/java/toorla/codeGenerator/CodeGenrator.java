d066ac403f7a83dcd24f44ac942c80db89c01c5d
zhpinkman <zhivarsourati@gmail.com>
2019-06-06 15:39:49 +0430

    jasmin jar file added to artifact for testing

--- src/main/java/toorla/codeGenerator/CodeGenrator.java
+++ src/main/java/toorla/codeGenerator/CodeGenrator.java
@@ -30,6 +30,12 @@ import toorla.types.singleType.StringType;
 import toorla.types.singleType.UserDefinedType;
 import toorla.utilities.graph.Graph;
 import toorla.visitor.Visitor;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -50,6 +56,41 @@ public class CodeGenrator extends Visitor<Void> {
     ExpressionTypeExtractor expressionTypeExtractor;
 //    public PrintWriter printWriter;
     int tabs_before;
+
+
+    public void copy_jasmin_file(){
+
+
+        InputStream inStream = null;
+        OutputStream outStream = null;
+
+        try{
+
+            File afile =new File("jasmin.jar");
+            File bfile =new File("artifact/jasmin.jar");
+
+            inStream = new FileInputStream(afile);
+            outStream = new FileOutputStream(bfile);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            //copy the file content in bytes
+            while ((length = inStream.read(buffer)) > 0){
+
+                outStream.write(buffer, 0, length);
+
+            }
+
+            inStream.close();
+            outStream.close();
+
+            System.out.println("File is copied successful!");
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
     public String get_access_modifier(String access_modifier){
         if (access_modifier.equals(PUBLIC_ACCESS))
@@ -153,6 +194,7 @@ public class CodeGenrator extends Visitor<Void> {
         try{
             theDir.mkdir();
             create_Any_class();
+            copy_jasmin_file();
         }
         catch(SecurityException se){
         }
